3b9e76525f8221fd4f85504f5cfbfd70fdc2bf73
byldh <byldh@sehanrf.co.kr>
2023-05-17 13:19:15 +0900

    git init

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.kotlin) apply false
+    alias(libs.plugins.android.kotlin.kapt) apply false
+    alias(libs.plugins.android.hilt) apply false
+    alias(libs.plugins.android.navigation.safeargs) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.android.kotlin.parcelize) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/comm.versions.toml"))
+        }
+    }
+}
+rootProject.name = "BeHealthy"
+include (":app")
